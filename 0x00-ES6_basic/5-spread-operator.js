7e7b59226fb24ce605f5258cb22560ca4bb135cd
Day-C <kongranalanyuy@gmail.com>
2024-10-03 23:50:03 +0000

    concatenating winth the spread operator

---
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -0,0 +1,5 @@
+export default function concatArrays(array1, array2, string) {
+	// use the spread operator to return a single list of all functions argument characters
+	const sprd = [...array1, ...array2, ...string];
+	return sprd
+}
