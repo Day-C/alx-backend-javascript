ea38bf31de9a41ace53a3e9f7aab7bcb5e404028
Day-C <kongranalanyuy@gmail.com>
2024-10-06 08:38:14 +0000

    using promise.all() to resolve multiple promises with a single resolve

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()]).then((response) => {
+    const upload = response[0];
+    const create = response[1];
+    // log consts
+    console.log(`${upload.body} ${create.firstName} ${create.lastName}`);
+  })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
