e7b7cee2f130fa8f535e8e0811332a3e96fa8721
Day-C <kongranalanyuy@gmail.com>
2024-10-12 22:51:32 +0000

    methods

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,16 @@
+export default class Currennry {
+  constructor(code, name) {
+    if (typeof (code) !== 'string') {
+      throw TypeError('code must be a string');
+    }
+    if (typeof (name) !== 'string') {
+      throw TypeError('name must be a string');
+    }
+    this._code = code;
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
