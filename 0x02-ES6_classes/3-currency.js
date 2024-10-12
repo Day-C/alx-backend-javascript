741bebcb3b259cd6eb8a00f5e0ed085800656301
Day-C <kongranalanyuy@gmail.com>
2024-10-12 23:06:33 +0000

    instance methods

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -10,6 +10,33 @@ export default class Currennry {
     this._name = name;
   }
 
+  // get set code
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (typeof (newCode) !== 'string') {
+      throw TypeError('code must be a string');
+    }
+    this._code = newCode;
+  }
+
+  // get set name
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof (newName) !== 'string') {
+      throw TypeError('name must be a string');
+    }
+    this._name = newName;
+  }
+
+  // intance method
+
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
