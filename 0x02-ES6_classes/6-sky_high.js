98eda1c249d0008308d34b0324f908a9f0a93d60
Day-C <kongranalanyuy@gmail.com>
2024-10-13 04:22:13 +0000

    inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,29 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    if (typeof (floors) !== 'number') {
+      throw TypeError('floors must be a number');
+    }
+    super(sqft);
+    this._floors = floors;
+  }
+
+  // get and set floors
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(newFloors) {
+    if (typeof (newFloors) !== 'number') {
+      throw TypeError('floors must be a number');
+    }
+    this._floors = newFloors;
+  }
+
+  evacuationWarningMessage() {
+    this.j2b = 0;
+    return 'Evacuate slowly the NUMBER_OF_FLOORS floors';
+  }
+}
