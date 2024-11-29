642fd6693cc68ba5944da8eba8978a22bc61c6db
DayC <kongranalanyuy@gmail.com>
2024-11-29 16:20:53 +0000

    cmd line programm

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,4 +1,18 @@
 // run on command line
-console.log("Welcome to Holberton School, what is your name?")
-console.log("Your name is: " + process.argv[2])
-console.log("This important software is now closing")
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
+  console.log(`Your name is: ${answer}`);
+  rl.close(() => {
+    console.log('This important software is now closing');
+  });
+});
+
+rl.on('close', () => {
+  console.log('This important software is now closing');
+});
