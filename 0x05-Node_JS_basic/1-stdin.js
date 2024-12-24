b9eb7813b7a5ba5f5fab60b0645f615abc7d2d4a
DayC <Kongranalanyuy@gmail.com>
2024-12-24 15:45:21 -0800

    STDIN and STDOUT with node

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
-// run on command line
+/* run on command line
 const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
@@ -15,4 +15,13 @@ rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
 
 rl.on('close', () => {
   console.log('This important software is now closing');
+});*/
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
