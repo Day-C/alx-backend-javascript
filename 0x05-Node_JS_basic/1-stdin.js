f3945dde4fc35d7192c902c36639b403c3ba6813
DayC <Kongranalanyuy@gmail.com>
2024-12-24 15:56:25 -0800

    STDIN and STDOUT with nde.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,22 +1,5 @@
- /* run on command line
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
-  console.log(`Your name is: ${answer}`);
-  rl.close(() => {
-    console.log('This important software is now closing');
-  });
-});
-
-rl.on('close', () => {
-  console.log('This important software is now closing');
-});*/
-
+// node for command line
+//
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
