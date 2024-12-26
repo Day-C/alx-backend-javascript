087914d5c3b1c305a62ededa07f57890131d064d
DayC <Kongranalanyuy@gmail.com>
2024-12-26 03:55:03 -0800

    servers with http

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -6,10 +6,9 @@ const hostname = '127.0.0.1';
 const port = 1245;
 const app = createServer();
 
-
-//
+// on request
 app.on('request', (_, res) => {
-  const responseTxt = 'Hello ALX!';
+  const responseTxt = 'Hello Holberton School!';
 
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', responseTxt.length);
@@ -18,7 +17,7 @@ app.on('request', (_, res) => {
 });
 
 app.listen(port, hostname, () => {
-  console.log(`app running at http://${hostname}:${port}/`);
+  process.stdout.write(`Server listening at -> http://${hostname}:${port}\n`);
 });
 
 module.exports = app;
