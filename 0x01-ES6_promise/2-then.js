7a9df6d7ab482b44ce78bd5ea62c69febfbf518a
DayC <Kongranalanyuy@gmail.com>
2024-12-28 05:15:17 -0800

    a promise as function arg

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then(() => {
-      // render on success
-      return { status: 200, body: 'success' };
+  return promise
+    .then(() => ({status: 200, body: 'success' });
     })
-    .catch(() => {
-      // catch failure
-      return new Error();
+    .catch(() => (new Error());
     })
-    .finally(() => {
-      // log regardless the resolution
-      console.log('Got a response from the API');
-    });
+    .finally(() => console.log('Got a response from the API'));
 }
