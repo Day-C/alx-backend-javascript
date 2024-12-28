4e374041fea793241f00dff82f6acde9811598ba
DayC <Kongranalanyuy@gmail.com>
2024-12-27 19:56:45 -0800

    iterate through object

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,14 +1,15 @@
 export default function iterateThroughObject(reportWithIterator) {
-  const names = reportWithIterator
+  const names = reportWithIterator;
   let count = 0;
-  let display = ``;
-  for (let name of names) {
+  let display = '';
+  // eslint-disable-next-line no-unused-vars, no-shadow
+  for (const name of names) {
     count += 1;
-    if (count == 1) {
+    if (count === 1) {
       display += `${name}`;
     } else {
-      display += ` | ${name}`
+      display += ` | ${name}`;
     }
   }
-  return display
+  return display;
 }
