d9a7390d2614321561c8d852fb76cadd0b483f1d
DayC <Kongranalanyuy@gmail.com>
2024-12-28 23:59:43 -0800

    try

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -2,8 +2,8 @@ export default function guardrail(mathFunction) {
   const queue = [];
   try {
     queue.push(mathFunction());
-  } catch (e) {
-    queue.push(e.message);
+  } catch (error) {
+    queue.push(String(error);
   } finally {
     queue.push('Guardrail was processed');
   }
