841222d3c4a2154948dc81b7bb7011da4dde3dcb
DayC <Kongranalanyuy@gmail.com>
2024-12-29 12:18:16 -0800

    await promise

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,6 +3,6 @@ import { createUser, uploadPhoto } from './utils';
 export default async function asyncUploadUser() {
   return Promise
     .all([await createUser(), await uploadPhoto()])
-    .then((results) => ({ photo: results[0], user: results[1] }))
+    .then((result) => ({ photo: result[1], user: result[0] }))
     .catch(() => ({ photo: null, user: null }));
 }
