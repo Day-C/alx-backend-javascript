d3d432299a97a4623a18c7fbfcfd8c08863d55de
DayC <Kongranalanyuy@gmail.com>
2024-12-30 18:41:21 -0800

    return mapp

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+/**
+ * retuns a map of groceries with items
+ */
+export default function groceriesList() {
+  const values = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+  return new Map(values);
+}
