adaf5c882d0434f955914188af94b8ad95a6335e
DayC <Kongranalanyuy@gmail.com>
2024-12-30 18:13:33 -0800

    sets

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,8 @@
+/**
+ * setFromArray: returs a set from an array argument
+ * Args: array(list containing values)
+ * Return: set
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
